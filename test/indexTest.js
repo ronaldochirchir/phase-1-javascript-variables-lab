4b32c5387fcde614121f47d57caedbdcc4f667fd
Ronaldo Kipkirui <ronaldochirchir8@gmail.com>
2024-11-28 08:15:49 +0300

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,7 +9,7 @@ describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
-    });
+    })
 
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
